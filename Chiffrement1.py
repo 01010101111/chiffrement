2523a4864d56a51cfd0eb0a3b7462929f5a685b3
01010101111 <53013063+01010101111@users.noreply.github.com>
2019-07-17 18:20:59 +0200

    Add files via upload

---
+++ Chiffrement1.py
@@ -0,0 +1,66 @@
+import time
+
+a = 53 
+b = 6758
+c = 7
+d = 67545
+e = 346
+f = 567
+g = 8976
+h = 3955
+i = 83
+j = 99 
+k = 555 
+l = 933
+m = 556
+n = 45
+o = 23
+p = 55543
+q = 9001
+r = 8111
+s = 5678
+t = 4444
+u = 33
+v = 13455
+w = 99111
+x = 6777
+y = 87
+z = 87678
+
+oui = 1
+
+non = 0
+
+
+print("commencer ? oui / non")
+
+choix=input()
+
+
+while choix == 1:
+     print("votre mot");
+
+
+
+     a1 = input()
+     a2 = input()
+     a3 = input()
+     a4 = input()
+     a5 = input()
+     a6 = input()
+     a7 = input()
+     a8 = input()
+
+     mot =a2 + a3 + a4 + a5 + a6 + a7 + a8
+
+
+     print(a1)
+
+     mot = (mot * a1) * 24 
+
+
+     print(mot)
+
+     print ("continuer ? oui / non")
+     choix = input()
+
---
+++ Chiffrement2.py
@@ -0,0 +1,20 @@
+
+print("phase 1") 
+
+phase1 = input()
+
+print("phase 2")
+
+phase2 = input()
+
+
+lettre2 = (phase2 / phase1) / 24 ##
+
+lettre3 = lettre2 / 24 ##
+
+lettre1 = phase1 
+
+print("lettre 1 : ", lettre1 )
+
+print ("valeur du mot : ", lettre3 )
+
